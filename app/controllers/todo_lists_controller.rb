0e63ea268cc8769912a8586a3524fee60de694a7
Nihaal Dhakal <nihaldhakal41@gmail.com>
2018-10-29 11:38:54 +0545

    Todolist can be shown,edited and updated

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,4 +1,6 @@
 class TodoListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit, :update]
+
   def index
     @list = TodoList.all
   end
@@ -20,11 +22,29 @@ class TodoListsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @list.update(todo_list_params)
+        format.html { redirect_to @list, notice: 'Race was successfully updated.' }
+        format.json { render :show, status: :ok, location: @list }
+      else
+        format.html { render :edit }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def show
-    @list = TodoList.find(params[:id])
   end
 
   private
+
+  def set_list
+    @list = TodoList.find(params[:id])
+  end
 
   def todo_list_params
     params.require(:todo_list).permit(:title, :description)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :todo_lists, only: [:index, :new, :create, :show]
+resources :todo_lists, only: [:index, :new, :create, :edit, :update, :show]
 root "todo_lists#index"
 end
